4f71371c02f2507fe03820dc22322b02a528e17c
nathansng <nathansng17@gmail.com>
2017-10-05 21:55:27 -0700

    Fixed subject intialization

--- Main.java
+++ Main.java
@@ -6,11 +6,7 @@ public class Main {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in); // Reading from System.in
-        
-        System.out.println("Choose subject: ");
-        System.out.println("Kinematics----------------1");
-        System.out.println("Forces--------------------2");
-        
+        titles(); //prints out all the titles (code below main)
         String choice = input.nextLine();
 
         int choiceNum = 0;
@@ -29,18 +25,24 @@ public class Main {
         if(isValidNum){
           switch(choiceNum){
             case 1:
-              Kinematics projectile = new Kinematics("Kinematics");
-              //System.out.println(projectile.getTitle());
+              Subject projectile = new Kinematics("Kinematics");
+              System.out.println(projectile.getTitle());
               projectile.typeOfQ();
               break;
             case 2:
-              Forces extortion = new Forces("Forces");
-              //System.out.println(projectile.getTitle());
+              Subject extortion = new Forces("Forces");
+              System.out.println(extortion.getTitle());
               extortion.typeOfQ();
               break;
           }
         }
         
 
+    }
+    
+    public static void titles () {
+      System.out.println("Choose subject: ");
+      System.out.println("Kinematics----------------1");
+      System.out.println("Forces--------------------2");
     }
 }
