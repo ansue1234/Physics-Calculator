3a0767ae37f249d962491fffe9ce48350469ac50
ansue1234 <ansue1234@gmail.com>
2017-10-05 20:14:20 -0700

    Testing

--- Main.java
+++ Main.java
@@ -21,30 +21,5 @@ public class Main {
             System.out.println("Number Format error");
         }
 
-        if (isValidNum) {
-            int amountInPennies = (int) Math.round(amountInDollars * 100);
-            amountInPennies = (int) (Math.round(amountInPennies / 5.0) * 5);
-
-            // toonies
-            int numberofToonies = (int) (amountInPennies / 200.00);
-            amountInPennies = amountInPennies % 200;
-            // loonies
-            int numberofLoonies = (int) (amountInPennies / 100.00);
-            amountInPennies = amountInPennies % 100;
-            // quarters
-            int numberofQuarters = (int) (amountInPennies / 25.00);
-            amountInPennies = amountInPennies % 25;
-            // dimes
-            int numberofDimes = (int) (amountInPennies / 10.00);
-            amountInPennies = amountInPennies % 10;
-            // nickels
-            int numberofNickels = (int) (amountInPennies / 5.00);
-
-            System.out.println("toonies:" + numberofToonies + ";" + " loonies:"
-                    + numberofLoonies + ";" + " quarters:" + numberofQuarters
-                    + ";" + " dimes:" + numberofDimes + ";" + " nickels:"
-                    + numberofNickels);
-        }
-
     }
 }
